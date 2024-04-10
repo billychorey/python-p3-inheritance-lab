dee736d9dc349d9453780f3cc94fe8ee2ff055c5
Billy <billychorey@gmail.com>
2024-04-09 20:41:56 -0400

    inheritance lab

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,17 @@
-#!/usr/bin/env python
+# student.py
 
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        """
+        Initialize a Student object.
+        """
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, subject):
+        """
+        Learn a new subject and add it to the knowledge list.
+        """
+        self.knowledge.append(subject)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,27 @@
-#!/usr/bin/env python
-
+import random
 from user import User
 
-import random
-
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+       
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
-        pass
+       
+        if not self.knowledge:
+            return "The teacher has no knowledge to teach."
+        return random.choice(self.knowledge)
+
+    def add_knowledge(self, knowledge):
+      
+        self.knowledge.append(knowledge)
